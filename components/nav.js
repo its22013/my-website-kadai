ff7ea55d7cf4b6e8e4bd9bd1ae7cf189c1955a43
its22013 <s22013@std-college.ac.jp>
2023-02-18 14:13:25 +0000

    first commit

---
+++ components/nav.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styles from 'styles/nav.module.css'
+
+export default function Nav () {
+  return (
+    <nav>
+      <ul className={styles.list}>
+        <li>
+          <Link legacyBehavior href='/'>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link legacyBehavior href='/about'>
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link legacyBehavior href='/blog'>
+            <a>Blog</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Container from 'components/container'
+import Hero from 'components/hero'
+
+export default function Blog () {
+  return (
+    <Container>
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+    </Container>
+  )
+}
